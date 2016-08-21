205244d0c8335af86e46b0f8dc94dd3201247139
ATrigger <kamenev@rain.ifmo.ru>
2016-08-21 04:07:29 +0300

    Implemented methonds on chat model

---
+++ app/chatTypes.js
@@ -0,0 +1,7 @@
+'use strict'
+//enum
+module.exports.chatTypes = {
+    private: {id: 1},
+    group: {id: 2},
+    supergroup: {id: 3}
+}
--- models/chat.js
+++ models/chat.js
@@ -1,15 +1,39 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
+const { chatTypes } = require('../app/chatTypes.js');
+
+module.exports = function (sequelize, DataTypes) {
   var Chat = sequelize.define('Chat', {
     telegram_id: DataTypes.INTEGER(8),
-    chat_type: DataTypes.INTEGER(1),
-    random_chance: DataTypes.INTEGER(2)
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
+    chat_type: DataTypes.ENUM('private', 'group', 'supergroup'),
+    random_chance: {
+      type: DataTypes.INTEGER(2),
+      defaultValue: 50
     }
-  });
+  }, {
+      classMethods: {
+        associate: function (models) {
+          Chat.hasMany(models.Pair);
+        },
+        getChat: function (tg_message) {
+          var chat = tg_message.chat;
+          var tg_id = chat.id;
+          return Chat.findOrCreate({
+            where: {
+              telegram_id: tg_id,
+              chat_type: chat.type
+            }, defaults: {}
+          });//promise??
+        }
+      },
+      instanceMethods: {
+        migration: function (chatId) {
+          this.update({ telegram_id: chatId })
+            .then(function (data) { console.log('Succesfully migrated to ' + chatId) })
+            .catch(function () {
+              console.log('Failed to migrate to chatId.');
+            });
+        }
+      }
+    });
   return Chat;
 };
