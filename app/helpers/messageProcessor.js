9c02fbdbd5a052e391861b8dc7ebac66f551afc7
Vladislav Kamenev <mazahakaforever@ya.ru>
2021-02-15 02:45:49 +0300

    Make RNG work

--- app/helpers/messageProcessor.js
+++ app/helpers/messageProcessor.js
@@ -63,7 +63,7 @@ class MessageProcessor {
 
   randomOK() {
     const rng = Math.floor(Math.random() * 100);
-    return true;
+    return rng >= this.chat.random_chance;
   }
 
   async generateAnswer() {
